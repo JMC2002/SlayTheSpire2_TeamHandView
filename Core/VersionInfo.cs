bdf8480d1c8f2c49a34efbbf7e3d608f47866904
MC J <jmc8@qq.com>
2026-04-30 22:52:00 +0800

    补齐本地化

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace TeamHandView.Core;
 public static class VersionInfo
 {
     public const string Name = "TeamHandView";
-    public const string Version = "1.0.7";
+    public const string Version = "1.0.8";
 
     public static string Tag => $"[{Name} v{Version}]";
 }
