88d82b7f748e12c6296af96f3426bfda3e27432a
MC J <jmc8@qq.com>
2026-05-15 01:56:43 +0800

    版本号增加

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace TeamHandView.Core;
 public static class VersionInfo
 {
     public const string Name = "TeamHandView";
-    public const string Version = "1.0.24";
+    public const string Version = "1.1.0";
 
     public static string Tag => $"[{Name} v{Version}]";
 }
